e380166e0c61d70f92d4234a3996e2f8514c1f3d
Michael Kulygin <misheltus1@gmail.com>
2026-07-29 23:15:20 +0300

    in proccess

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 
+from django.core.exceptions import ValidationError
+
 from .models import  Birthday
+
+BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
 
 class BirthdayForm(forms.ModelForm):
 
@@ -8,6 +12,20 @@ class BirthdayForm(forms.ModelForm):
         model = Birthday
         fields = '__all__'
         widgets = {
-            'birthday': forms.DateInput(attrs={'type': 'date'})
+            'birthday': forms.DateInput(attrs={'type': 'date'}),
         }
 
+    def clean_first_name(self):
+        # Получаем значение имени из словаря очищенных данных.
+        first_name = self.cleaned_data['first_name']
+        # Разбиваем полученную строку по пробелам
+        # и возвращаем только первое имя.
+        return first_name.split()[0]
+
+    def clean(self):
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        if f'{first_name} {last_name}' in BEATLES:
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            )
--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from .validators import real_age
 
 class Birthday(models.Model):
     first_name = models.CharField('Имя', max_length=20)
@@ -9,4 +10,4 @@ class Birthday(models.Model):
         help_text='Необязательное поле',
         max_length=20
     )
-    birthday = models.DateField('Дата рождения')
+    birthday = models.DateField('Дата рождения', validators=(real_age,))
