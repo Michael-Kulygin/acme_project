d58447568ef21b0f225390edb12d51207f2e1c53
Michael Kulygin <misheltus1@gmail.com>
2026-08-03 14:22:26 +0300

    in proccess

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404 ,render, redirect
+from django.core.paginator import Paginator
 
 from .forms import BirthdayForm
 from .utils import calculate_birthday_countdown
@@ -6,10 +7,11 @@ from .models import Birthday
 
 
 def birthday_list(request):
-    # Получаем все объекты модели Birthday из БД.
-    birthdays = Birthday.objects.all()
-    # Передаём их в контекст шаблона.
-    context = {'birthdays': birthdays}
+    birthdays = Birthday.objects.order_by('id')
+    paginator = Paginator(birthdays, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'page_obj': page_obj}
     return render(request, 'birthday/birthday_list.html', context)
 
 def birthday(request, pk=None):
