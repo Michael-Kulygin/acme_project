743afb0db57e3b58ea010afb8ecc6e3b39d38095
Michael-Kulygin <misheltus1@gmail.com>
2026-08-07 14:39:33 +0300

    in proccess

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -9,6 +9,8 @@ from django.views.generic.edit import CreateView
 from django.urls import include, path, reverse_lazy
 
 
+handler404 = 'core.views.page_not_found'
+
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
